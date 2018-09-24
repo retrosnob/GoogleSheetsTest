12d04cd4ef48a26d75f141d9f27fdf4f0bb6b02c
justinrobertson <jusrobertson@gmail.com>
2018-09-24 12:32:36 +0800

    Added the update vs append methods

--- src/main/java/googlesheetstest/Main.java
+++ src/main/java/googlesheetstest/Main.java
@@ -13,12 +13,16 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,12 +31,13 @@ public class Main {
     private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static Sheets service;
 
     /**
      * Global instance of the scopes required by this quickstart. If modifying
      * these scopes, delete your previously saved tokens/ folder.
      */
-    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
     public static void main(String... args) throws IOException, GeneralSecurityException {
@@ -43,8 +48,8 @@ public class Main {
         final String spreadsheetId = "1upY5Zq2zbgWb9-BkJSnqi5JukW07cAgipW3OgicM3f0"; 
         // ******************************************************************************   
         
-        final String range = "Sheet1!A1";
-        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        final String range = "Test!A1";
+        service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
         ValueRange response = service.spreadsheets().values()
@@ -56,8 +61,60 @@ public class Main {
         } else {
             System.out.println(values.get(0));
         }
+//        UpdateValuesResponse updateResponse = updateValues(spreadsheetId, range);
+        AppendValuesResponse appendResponse = appendValues(spreadsheetId, range);
+    }
+    
+    public static UpdateValuesResponse updateValues(String spreadsheetId, String range)
+            throws IOException {
+        
+        List<List<Object>> values = new ArrayList();
+        values.add(new ArrayList());
+        values.get(0).add("Jackdaws");
+        values.get(0).add("love");
+        values.get(0).add("my");
+        values.get(0).add("big");
+        
+        ValueRange body = new ValueRange()
+                .setValues(values);
+        UpdateValuesResponse result =
+                service.spreadsheets().values().update(spreadsheetId, range, body)
+                        .setValueInputOption("USER_ENTERED")
+                        .execute();
+        System.out.printf("%d cells updated.", result.getUpdatedCells());
+
+        // [END sheets_update_values]
+        return result;
     }
 
+        public static AppendValuesResponse appendValues(String spreadsheetId, String range)
+            throws IOException {
+        
+        List<List<Object>> values = new ArrayList();
+        values.add(new ArrayList());
+        values.get(0).add("Jackdaws");
+        values.get(0).add("love");
+        values.get(0).add("my");
+        values.get(0).add("big");
+        
+
+        
+        ValueRange body = new ValueRange()
+                .setValues(values);
+//        UpdateValuesResponse result =
+//                service.spreadsheets().values().update(spreadsheetId, range, body)
+//                        .setValueInputOption("USER_ENTERED")
+//                        .execute();
+        // System.out.printf("%d cells updated.", result.getUpdatedCells());
+
+        AppendValuesResponse result =
+                service.spreadsheets().values().append(spreadsheetId, range, body)
+                        .setValueInputOption("USER_ENTERED")
+                        .execute();
+        // [END sheets_update_values]
+        return result;
+    }
+    
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         InputStream in = Main.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
